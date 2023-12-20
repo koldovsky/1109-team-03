732d03f64d7807b79668b7c5d1b02c1c830e0bfd
litvischenkoev <litvischenkoev@gmail.com>
2023-12-20 12:06:20 +0200

    Update tasks-02.js

--- js-tasks/tasks-02.js
+++ js-tasks/tasks-02.js
@@ -19,3 +19,27 @@ function DNAtoRNA(dna) {
 function DNAtoRNA(dna) {
   return dna.replace(/T/g, "U");
 }
+
+//3 https://www.codewars.com/kata/577a98a6ae28071780000989/train/javascript
+var min = function(list){
+  for (var i=1;i<list.length;i++){
+    if (list[0]>list[i]) list[0]=list[i];      
+    }
+    return list[0];
+}
+
+var max = function(list){
+    for (var i=1;i<list.length;i++){
+    if (list[0]<list[i]) list[0]=list[i];      
+    }
+    return list[0];
+}
+//4 https://www.codewars.com/kata/544a54fd18b8e06d240005c0/train/javascript
+function min(arr, toReturn) {
+  if (toReturn === 'value'){
+    return Math.min(...arr);
+  }
+  if (toReturn === 'index'){
+    return arr.indexOf(Math.min(...arr));
+  }
+}
