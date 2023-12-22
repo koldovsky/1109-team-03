301d92b46963fcc45df4564e41851a6e4df3178b
litvischenkoev <litvischenkoev@gmail.com>
2023-12-22 09:14:55 +0200

    Update task-03.js

--- js-tasks/task-03.js
+++ js-tasks/task-03.js
@@ -10,3 +10,13 @@ class SmallestIntegerFinder {
 function circleCircumference(circle) {
     return circle.radius * Math.PI * 2;  
   };
+
+//3 https://discord.com/channels/1173634356514914324/1177309471408672829/1187099239092850699
+function giveMeFive(obj){
+  rez = []
+  for (var key in obj) {
+    if (key.length === 5) rez.push(key);
+    if (obj[key].length === 5) rez.push(obj[key]);
+  }
+  return rez;
+}
