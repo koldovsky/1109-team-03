0b00215a8a51d575dedbfb382b000a0af0d5c9ce
litvischenkoev <litvischenkoev@gmail.com>
2023-12-24 13:16:49 +0000

    main-new update

--- script/index.js
+++ script/index.js
@@ -1,3 +1,5 @@
 document.addEventListener('partialsLoaded', () => {
-    import('./main-new.js');
+    import('./main-new-tab.js');
+    // import('./main-new-tab1.js');
+    import('./main-new-carousel.js');
 });
---
+++ script/main-new-tab.js
@@ -0,0 +1,12 @@
+const tabs = document.querySelectorAll('.tab__product');
+const items = document.querySelectorAll('.catalog__items');
+
+tabs.forEach((tab, index) => {
+  tab.addEventListener('click', () => {
+    tabs.forEach(t => t.classList.remove('active'));
+    items.forEach(item => item.classList.remove('active'));
+
+    tab.classList.add('active');
+    items[index].classList.add('active');
+  });
+});
