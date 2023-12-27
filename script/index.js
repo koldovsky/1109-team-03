915c16da59d627c8c510fefe470b80c8410eea23
Danylo-Matiunin <152752032+Danylo-Matiunin@users.noreply.github.com>
2023-12-27 19:32:48 +0000

    js

--- script/index.js
+++ script/index.js
@@ -1,9 +1,6 @@
 document.addEventListener('partialsLoaded', () => {
     import('./main-new-tab.js');
     import('./main-new-carousel.js');
-<<<<<<< HEAD
     import('./footer-gallery.js');
-=======
     import('./main-passion.partial.js');
->>>>>>> 8c39bae (js)
 });
