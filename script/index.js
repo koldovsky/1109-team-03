11880a317adb80245b4314f7384fd108dfbd936d
OlegKovalovsky <151868569+OlegKovalovsky@users.noreply.github.com>
2024-01-05 20:39:40 +0000

    upload-about-us

--- script/index.js
+++ script/index.js
@@ -3,4 +3,5 @@ document.addEventListener('partialsLoaded', () => {
     import('./main-new-carousel.js');
     import('./footer-gallery.js');
     import('./main-passion.partial.js');
+    import('./main-about-download.js')
 });
--- script/main-about-download.js
+++ script/main-about-download.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const downloadButton = document.getElementById('download-button');
 
     downloadButton.addEventListener('click', function () {
-        const pdfUrl = 'img/About-us.pdf';
+        const pdfUrl = 'img/about-us.pdf';
         const downloadLink = document.createElement('a');
         downloadLink.href = pdfUrl;
         downloadLink.download = 'About-us.pdf';
