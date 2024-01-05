40bb58cbe4a80a43cd82092554fb7ee43ed44f96
OlegKovalovsky <151868569+OlegKovalovsky@users.noreply.github.com>
2024-01-05 19:43:34 +0000

    updete-about-us

---
+++ script/main-about-download.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const downloadButton = document.getElementById('download-button');
+
+    downloadButton.addEventListener('click', function () {
+        const pdfUrl = 'img/about-us.pdf';
+        const downloadLink = document.createElement('a');
+        downloadLink.href = pdfUrl;
+        downloadLink.download = 'about-us.pdf';
+        downloadLink.click();
+    });
+});
