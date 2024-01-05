4f28dacb160f39afee594a594a920521ef19c86d
OlegKovalovsky <151868569+OlegKovalovsky@users.noreply.github.com>
2024-01-05 20:10:32 +0000

    changes-html

--- script/main-about-download.js
+++ script/main-about-download.js
@@ -2,10 +2,10 @@ document.addEventListener('DOMContentLoaded', function () {
     const downloadButton = document.getElementById('download-button');
 
     downloadButton.addEventListener('click', function () {
-        const pdfUrl = 'img/about-us.pdf';
+        const pdfUrl = 'img/About-us.pdf';
         const downloadLink = document.createElement('a');
         downloadLink.href = pdfUrl;
-        downloadLink.download = 'about-us.pdf';
+        downloadLink.download = 'About-us.pdf';
         downloadLink.click();
     });
 });
