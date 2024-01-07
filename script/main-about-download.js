d2649535a36d2abf7c829bdd90c933410ba033d5
OlegKovalovsky <151868569+OlegKovalovsky@users.noreply.github.com>
2024-01-07 13:36:17 +0000

    updete-js-about-us

--- script/main-about-download.js
+++ script/main-about-download.js
@@ -1,7 +1,7 @@
     const downloadButton = document.getElementById('download-button-pdf');
 
     downloadButton.addEventListener('click', function () {
-        const pdfUrl = '.about-us.pdf';
+        const pdfUrl = 'img/about-us.pdf';
         const downloadLink = document.createElement('a');
         downloadLink.href = pdfUrl;
         downloadLink.download = 'about-us.pdf';
